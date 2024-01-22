1a5fb5de6eac941d4b9788464ba5c3b8c92c5d48
warclock2 <152826398+warclock2@users.noreply.github.com>
2024-01-21 20:58:13 -0500

    Add files via upload

---
+++ consumo de api/index.js
@@ -0,0 +1,32 @@
+function getCharacters(done){
+    const results =fetch("https://rickandmortyapi.com/api/character");
+
+    results
+      .then (Response => Response.json())
+      .then (data => {
+        done(data)
+      });
+}
+
+getCharacters(data => {
+   data.results.forEach(personaje => {
+
+       const article = document.createRange().createContextualFragment(/*html*/`
+       
+       <article>
+
+         <div class="image-container">
+            <img src="${personaje.image}" alt="Personaje">
+         </div>
+
+         <h2>${personaje.name}</h2>
+         <span>${personaje.status}</span>
+
+       </article>
+       `);
+    const main = document.querySelector("main");
+
+    main.append(article);
+    
+   });
+});
